62b8a50fe779596df3c030388c7309229f6434d4
Julie Xue <j23xue@uwaterloo.ca>
2015-02-01 18:54:35 -0700

    first commit

--- js/makeSquare.js
+++ js/makeSquare.js
@@ -1,3 +1,13 @@
+var height = 20;
+var width = 10;
+var gridSize = 25;
+
 window.onload = function(){
+	//set the grid size
+	var canvasObj = document.getElementById("grid");
+	canvasObj.width = width * gridSize;
+	canvasObj.height = height * gridSize;
+
+	//draw lines
 	
 }
